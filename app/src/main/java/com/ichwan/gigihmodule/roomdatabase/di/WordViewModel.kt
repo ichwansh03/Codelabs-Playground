9b0c21ed159e80ad64471690993e68cc5fdefd33
ichwansh03 <ichwansholihin03@gmail.com>
2023-08-03 16:50:28 +0700

    room database: change viewModelScope to CoroutineScope(Dispatcher.IO)

--- app/src/main/java/com/ichwan/gigihmodule/roomdatabase/di/WordViewModel.kt
+++ app/src/main/java/com/ichwan/gigihmodule/roomdatabase/di/WordViewModel.kt
@@ -4,15 +4,17 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import com.ichwan.gigihmodule.roomdatabase.data.Word
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class WordViewModel(private val repository: WordRepository) : ViewModel() {
 
     val allWords: LiveData<List<Word>> = repository.allWords.asLiveData()
 
-    fun insert(word: Word) = viewModelScope.launch {
+    //Cannot access database on the main thread since it may potentially lock the UI for a long period of time
+    fun insert(word: Word) = CoroutineScope(Dispatchers.IO).launch {
         repository.insert(word)
     }
 }
