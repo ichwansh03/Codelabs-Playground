95b2c313e645228846c2098e609f8e5a83ded772
ichwansh03 <ichwansholihin03@gmail.com>
2023-08-03 16:49:13 +0700

    room database: delete suspend in function

--- app/src/main/java/com/ichwan/gigihmodule/roomdatabase/data/WordDao.kt
+++ app/src/main/java/com/ichwan/gigihmodule/roomdatabase/data/WordDao.kt
@@ -1,7 +1,7 @@
 package com.ichwan.gigihmodule.roomdatabase.data
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
-import com.ichwan.gigihmodule.roomdatabase.data.Word
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -11,8 +11,8 @@ interface WordDao {
     fun getAlphabetizedWords(): Flow<List<Word>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(word: Word)
+    fun insert(word: Word)
 
     @Query("DELETE FROM word_table")
-    suspend fun deleteAll()
+    fun deleteAll()
 }
