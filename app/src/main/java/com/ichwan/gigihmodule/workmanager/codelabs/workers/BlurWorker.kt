a2195dcb8b092ea25fdb7b7748dbb644e767a65a
ichwansh03 <ichwansholihin03@gmail.com>
2023-08-04 08:28:54 +0700

    work manager: add BlurWorker.kt

---
+++ app/src/main/java/com/ichwan/gigihmodule/workmanager/codelabs/workers/BlurWorker.kt
@@ -0,0 +1,51 @@
+package com.ichwan.gigihmodule.workmanager.codelabs.workers
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.text.TextUtils
+import android.util.Log
+import androidx.work.ListenableWorker
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import com.google.common.util.concurrent.ListenableFuture
+import com.ichwan.gigihmodule.workmanager.codelabs.KEY_IMAGE_URI
+import java.lang.Thread.sleep
+
+private const val TAG = "BlurWorker"
+class BlurWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+
+    override fun doWork(): Result {
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+        createStatusNotification("Blurring image", applicationContext)
+
+        sleep()
+
+        return try {
+            if (TextUtils.isEmpty(resourceUri)) {
+                Log.e(TAG, "Invalid input uri")
+                throw IllegalArgumentException("Invalid input uri")
+            }
+
+            val resolver = applicationContext.contentResolver
+
+            val picture = BitmapFactory.decodeStream(
+                resolver.openInputStream(Uri.parse(resourceUri)))
+
+            val output = blurBitmap(picture, applicationContext)
+
+            // Write bitmap to a temp file
+            val outputUri = writeBitmapToFile(applicationContext, output)
+
+            val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+
+            Result.success(outputData)
+        } catch (throwable: Throwable) {
+            Log.e(TAG, "Error applying blur")
+            throwable.printStackTrace()
+            Result.failure()
+        }
+
+    }
+}
