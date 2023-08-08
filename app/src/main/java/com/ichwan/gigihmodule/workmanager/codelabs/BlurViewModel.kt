b8bf893852b0b5f0f4f84a7728404baaab9e93e8
ichwansh03 <ichwansholihin03@gmail.com>
2023-08-09 05:50:59 +0700

    work manager: work request with number requested

--- app/src/main/java/com/ichwan/gigihmodule/workmanager/codelabs/BlurViewModel.kt
+++ app/src/main/java/com/ichwan/gigihmodule/workmanager/codelabs/BlurViewModel.kt
@@ -55,14 +55,22 @@ class BlurViewModel(application: Application) : ViewModel() {
 
         //add WorkRequest to Cleanup temporary images
         var continuation = workManager
-            .beginWith(OneTimeWorkRequest.Companion.from(CleanupWorker::class.java))
+            .beginUniqueWork(
+                IMAGE_MANIPULATION_WORK_NAME,
+                ExistingWorkPolicy.REPLACE,
+                OneTimeWorkRequest.from(CleanupWorker::class.java)
+            )
 
-        //add WorkRequest to blur the image
-        val blurRequest = OneTimeWorkRequestBuilder<BlurWorker>()
-            .setInputData(createInputDataForUri())
-            .build()
+        //add WorkRequest to blur the image with number of times requested
+        for (i in 0 until blurLevel) {
+            val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
 
-        continuation = continuation.then(blurRequest)
+            if (i == 0) {
+                blurBuilder.setInputData(createInputDataForUri())
+            }
+
+            continuation = continuation.then(blurBuilder.build())
+        }
 
         //add WorkRequest to save the image to the filesystem
         val save = OneTimeWorkRequest.Builder(SaveImageToFileWorker::class.java).build()
